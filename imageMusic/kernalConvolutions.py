18d239b583f88a72b594c3e972a6d04561854984
Nragis <Quinn_murphey@me.com>
2020-03-28 19:06:29 -0500

    Initialized kernalConvolutions.py

---
+++ imageMusic/kernalConvolutions.py
@@ -0,0 +1,17 @@
+class KernalConvolutions(object):
+
+    __height = 0
+    __width = 0
+    __weights = 0
+
+    def __init__(self, height, width, weights):
+        pass
+
+    def apply_filter():
+        pass
+
+    def change_filter():
+        pass
+
+    def check_filter_dimensions():
+        pass
