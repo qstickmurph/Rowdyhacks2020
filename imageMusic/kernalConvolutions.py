5210e27fa1d4c8dc8c835a4c3d72cc8b5b2a6fd8
Nragis <Quinn_murphey@me.com>
2020-03-28 20:34:56 -0500

    Added methods to kernalConvolutions class

--- imageMusic/kernalConvolutions.py
+++ imageMusic/kernalConvolutions.py
@@ -2,16 +2,25 @@ class KernalConvolutions(object):
 
     __height = 0
     __width = 0
-    __weights = 0
+    __weights = []
 
     def __init__(self, height, width, weights):
-        pass
+        self.__height = height
+        self.__width = width
+        self.__weights = weights
 
     def apply_filter():
-        pass
+        
 
-    def change_filter():
-        pass
+        return []
 
-    def check_filter_dimensions():
-        pass
+    def change_filter(self, weights):
+        self.__weights = weights
+
+    def check_filter_dimensions(self):
+        if len(self.__weights) != self.__height:
+            return False
+        for line in self.__weights:
+            if len(line) != self.__width:
+                return False
+        return True
