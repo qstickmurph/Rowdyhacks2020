cd1f7d219f7489a4c3d5bddfaf8bbd4359a5dfc0
Nragis <Quinn_murphey@me.com>
2020-03-29 05:22:54 -0500

    removed __ from class variable names in kernalConvolutions.py

--- imageMusic/kernalConvolutions.py
+++ imageMusic/kernalConvolutions.py
@@ -1,13 +1,13 @@
 class KernalConvolutions(object):
 
-    __height = 0 #how many pixels above and below the center the filter references
-    __width = 0 #how many pixels left and right of the center the filter references
-    __weights = [] #the list of weights for the relative positions of the pixels to the center
+    height = 0 #how many pixels above and below the center the filter references
+    width = 0 #how many pixels left and right of the center the filter references
+    weights = [] #the list of weights for the relative positions of the pixels to the center
 
-    def __init__(self, height, width, weights):
-        self.__height = height
-        self.__width = width
-        self.__weights = weights
+    def init(self, height, width, weights):
+        self.height = height
+        self.width = width
+        self.weights = weights
 
     def apply_filter(self, image):
         """Applies the filter defined by the object to the image. Assumes the image is a 2D list of integers"""
@@ -17,12 +17,12 @@ class KernalConvolutions(object):
             for j in range(0, len(image[i])): #loops over ever pixel in that line
                 weightedSum = 0
                 sumCount = 0
-                for y in range(max(0, j - self.__height), min(len(image[i]) - 1, j + self.__height) + 1):
+                for y in range(max(0, j - self.height), min(len(image[i]) - 1, j + self.height) + 1):
                     #loops over each horizontal line referenced by the filter
-                    for x in range(max(0, i - self.__width), min(len(image) - 1, i + self.__width) + 1):
+                    for x in range(max(0, i - self.width), min(len(image) - 1, i + self.width) + 1):
                         #loops over each pixel referenced by the filter in the line and adds it's value times the corrosponding weight to the weighted sum
-                        weightedSum += image[y][x]*self.__weights[y - max(0, j - self.__height)][x - max(0, i - self.__width)]
-                        sumCount += self.__weights[y - max(0, j - self.__height)][x - max(0, i - self.__width)]
+                        weightedSum += image[y][x]*self.weights[y - max(0, j - self.height)][x - max(0, i - self.width)]
+                        sumCount += self.weights[y - max(0, j - self.height)][x - max(0, i - self.width)]
                 #writes the average weighted value of the pixels to the final array
                 finalArray[j][i] = (int)(weightedSum/sumCount)
         return finalArray
@@ -30,10 +30,10 @@ class KernalConvolutions(object):
     def check_filter_dimensions(self):
         """Checks to see if the weights array is the correct dimensions. Returns True if it is, and False otherwise"""
 
-        if len(self.__weights) != self.__height*2 + 1:
+        if len(self.weights) != self.height*2 + 1:
             return False
-        for line in self.__weights:
-            if len(line) != self.__width*2 + 1:
+        for line in self.weights:
+            if len(line) != self.width*2 + 1:
                 return False
         return True
 
