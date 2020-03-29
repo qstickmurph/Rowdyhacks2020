b565514f2c3dd1d906cd51cebd6e81557ed622c1
davidtheboss <dgellhausen@gmail.com>
2020-03-29 02:05:07 -0500

    GUI can now import a photo and display it (but img needs to be resized)

--- imageMusic/baseUi.py
+++ imageMusic/baseUi.py
@@ -1,8 +1,40 @@
 import sys
 import os
-from tkinter import *
 
-def gui(title, message, path):
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import filedialog
+
+def UploadAction(event = None):
+    filename = filedialog.askopenfilename()
+    print('Selected:', filename)
+    load = Image.open(filename)
+    render = ImageTk.PhotoImage(load)
+    img = Label(image=render)
+    img.image = render
+    img.place(x=0, y=0)
+
+def gui(title, message):
     root = Tk()
-    w= 600
-    h= 800
+    root.title(title)
+
+    w= 450
+    h= 300
+    sw = root.winfo_screenwidth()
+    sh = root.winfo_screenheight()
+    x = (sw - w) / 2
+    y = (sh - h) / 2
+
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    m = message
+    m += '\n'
+
+    w = Label(root, text=m, width=120, height=10)
+    w.pack()
+    importbut = Button(root, text="Import", command=UploadAction, width=10)
+    importbut.pack()
+
+    mainloop()
+
+gui("GUI.pi", "Import an image file.")
