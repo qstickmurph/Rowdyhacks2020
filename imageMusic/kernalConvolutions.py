af629f033fb8e1f5c9fe3a56ed796e324b0ef8b9
Nragis <Quinn_murphey@me.com>
2020-03-28 21:07:38 -0500

    Wrote out for loops in apply_filter()

--- imageMusic/kernalConvolutions.py
+++ imageMusic/kernalConvolutions.py
@@ -9,18 +9,25 @@ class KernalConvolutions(object):
         self.__width = width
         self.__weights = weights
 
-    def apply_filter():
+    def apply_filter(self, image):
         
-
-        return []
+        finalArray = [[0 for j in range(0, image[i])] for i in range(0, len(image))]
+        for i in range(0, len(image)):
+            for j in range(0, len(image[i])):
+                weightedSum = 0
+                for x in range(max(0, i - self.__width), min(len(image) - 1), i + self.__width):
+                    for y in range(max(0, j - self.__height), min(len(image[i] - 1, j + self.__height))):
+                        weightedSum += image[y][x]*weights[y - max(0, j - self.__height)][x - max(0, i - self.__width)]        
+                finalArray[j][i] = weightedSum
+        return finalArray
 
     def change_filter(self, weights):
         self.__weights = weights
 
     def check_filter_dimensions(self):
-        if len(self.__weights) != self.__height:
+        if len(self.__weights) != self.__height*2 + 1:
             return False
         for line in self.__weights:
-            if len(line) != self.__width:
+            if len(line) != self.__width*2 + 1:
                 return False
         return True
