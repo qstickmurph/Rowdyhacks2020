f19e45b7f73af225f8b9c7ff1c15201beba7687a
Nragis <Quinn_murphey@me.com>
2020-03-29 05:30:27 -0500

    Finished initializing image.py and the Image class

---
+++ imageMusic/image.py
@@ -0,0 +1,24 @@
+class Image(object):
+
+    rawImage = None
+    height = 0
+    width = 0
+    rgbList = []
+    gsList = []
+
+    def __init__(self, image):
+        self.rawImage = image
+        self.rgbList = self.convert_to_rgb(self.rawImage)
+        self.gsList = self.convert_to_gs(self.rawImage)
+
+    def rgb(self):
+        return self.rgbList
+
+    def gs(self):
+        return self.gsList
+
+    def convert_to_rgb(self, image):
+        pass
+
+    def convert_to_gs(self, image):
+        pass
