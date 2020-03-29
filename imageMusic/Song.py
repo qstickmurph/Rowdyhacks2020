d3a10cd0582d59f221bac2141f34e27b8953e9ec
DezNova <47565853+DezNova@users.noreply.github.com>
2020-03-29 03:04:33 -0500

    add chord making static method

--- imageMusic/Song.py
+++ imageMusic/Song.py
@@ -26,38 +26,65 @@ class Song:
 	def instrumentation(self, value):
 		pass
 	
-	#@staticmethod
+	@staticmethod
+	def construct_chord(audio_1, audio_2, audio_3):
+		new_sound = AudioSegment.empty()
+		if len(audio_1) > len(audio_2):
+			new_sound = audio_1.overlay(audio_2)
+		else:
+			new_sound = audio_2.overlay(audio_1)
+			
+		if len(new_sound) > len(audio_3):
+			new_sound = new_sound.overlay(audio_3)
+		else:
+			new_sound = audio_3.overlay(new_sound)
+		return new_sound
+	
+	#TODO: Algorithm for choosing proper key
+	#TODO: Eliminate redundency
+	@staticmethod
 	def note( value, key=1, octave=1):
 		#select audio file based on value and key
 		#placeholder way to select nodes ( currently independent of key )
+		tk = 9
 		note = ""
-		print(str(value))
-		if value % 7 == 0:
+		if value % tk == 0:
 			note = "ff.C4.wav"
-		elif value % 7 == 1:
+		elif value % tk == 1:
 			note = "ff.B4.wav"
-		elif value % 7 == 2:
+		elif value % tk == 2:
 			note = "ff.A4.wav"
-		elif value % 7 == 3:
+		elif value % tk == 3:
 			note = "ff.E4.wav"
-		elif value % 7 == 4:
+		elif value % tk == 4:
 			note = "ff.F4.wav"
-		elif value % 7 == 5:
+		elif value % tk == 5:
 			note = "ff.G4.wav"
-		else:
+		elif value % tk == 6:
 			note = "ff.D4.wav"
-		#
-		audio_name = "sounds/"
-		audio_name = audio_name + str(octave) + "/" + note
-		file = open(audio_name, "r")
+		else: #for now this else conditions is tasked with building chords
 		
+			note_1 = "ff.C4.wav"
+			note_2 = "ff.E4.wav"
+			note_3 = "ff.G4.wav"
+		
+			audio_name_1 = "sounds/" + str(octave) + "/" + note_1
+			audio_name_2 = "sounds/" + str(octave) + "/" + note_2
+			audio_name_3 = "sounds/" + str(octave) + "/" + note_3
+			
+			audio_1 = AudioSegment.from_file(audio_name_1, format="wav")
+			audio_2 = AudioSegment.from_file(audio_name_2, format="wav")
+			audio_3 = AudioSegment.from_file(audio_name_3, format="wav")
+			
+			return Song.construct_chord(audio_1, audio_2, audio_3)
+		audio_name = "sounds/" + str(octave) + "/" + note
 		return AudioSegment.from_file(audio_name, format="wav")
+		
 	
 	def concatenate_melody(self, pixel_list):
 		octave = 1
 		sound = AudioSegment.empty() #initialize empty audio segment
 		for i in range(len(pixel_list)):
-			print(pixel_list[i])
 			sound = sound + Song.note(value=pixel_list[i], key=self.key, octave=1)
 		self.song = sound
 	
